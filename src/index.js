b79c191150c5a5eef60b8931518905839ead465a
Austin Varnon <austinvarnon@gmail.com>
2020-09-12 23:49:50 -0500

    finished all bonuses for npm exercises

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 "use strict";
+const $ = require('jquery');
+//TODO: receiving an error saying require is not defined.
+//TODO: added the build:watch script, ran it, does not rebuild upon changes in HTML.
 
 const sayHello = () => {
     console.log('Hello world');
 }
 
-sayHello();
+$('#hello').click(function (){
+    alert("Hi");
+})
+
+let hello = document.getElementById('hello-world');
+
+
